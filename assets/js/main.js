a5b933d2d1ac81d3a0f5ddc8fb3b1cfde8c7760d
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-10 15:36:47 +0200

    multiples of 5 and 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,15 +5,19 @@ for (let i = 1; i <= 100; i++){
 
 // stampare Fizz per numeri multipli di 3
 //stampare Buzz per numeri multipli di 5
+// stampare FizzBuzz per numeri multipli sia di 3 che di 5
+
 for (let i = 1; i <= 100; i++){
     console.log(i);
-    if (i % 3 == 0){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log('FizzBuzz');
+    } else if (i % 3 == 0){
         console.log('Fizz');
-    } else if (i % 5 == 0){
+    } else if (i % 5 === 0){
         console.log('Buzz');
-    }
+    }    
 }
 
 
-// stampare FizzBuzz per numeri multipli sia di 3 che di 5
+
 
