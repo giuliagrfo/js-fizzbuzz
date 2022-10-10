970b33d5a53724573f555467f4ebb6d9faaaace4
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-10 17:41:58 +0200

    fix-code final commit

--- bonus/assets/js/main_b.js
+++ bonus/assets/js/main_b.js
@@ -11,26 +11,24 @@ const listElement = document.querySelector('ul.list');
 for (let i = 1; i <= 100; i++){
     const ul = document.querySelector('ul.list');
     const li = document.createElement('li');
+    ul.append(li);
+
     // console.log(i);
     if (i % 3 === 0 && i % 5 === 0){
         li.className = 'fizzbuzz'
         li.append('Fizzbuzz');
-        ul.append(li);
         // console.log('FizzBuzz');
     } else if (i % 3 == 0){
         li.className = 'fizz'
         li.append('Fizz');
-        ul.append(li);
         // console.log('Fizz');
     } else if (i % 5 === 0){
         li.className = 'buzz'
         li.append('Buzz');
-        ul.append(li);
         // console.log('Buzz');
     } else {
         li.className = 'number'
         li.append(`${i}`);
-        ul.append(li);
     }
     
 }
