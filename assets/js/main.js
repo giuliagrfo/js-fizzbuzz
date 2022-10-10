18aec382a9cdcbe89cd2a58d6548fff5282b271d
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-10 15:18:07 +0200

    prints multiples of 3, multiples of 5

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,19 @@
 // stampare numeri da 1 a 100
 for (let i = 1; i <= 100; i++){
     console.log(i);
-}
+}   
 
 // stampare Fizz per numeri multipli di 3
-
 //stampare Buzz per numeri multipli di 5
+for (let i = 1; i <= 100; i++){
+    console.log(i);
+    if (i % 3 == 0){
+        console.log('Fizz');
+    } else if (i % 5 == 0){
+        console.log('Buzz');
+    }
+}
+
 
 // stampare FizzBuzz per numeri multipli sia di 3 che di 5
+
