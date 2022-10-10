383bb290107758c7be339febb082b8ad614b8051
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-10 16:48:29 +0200

    bonus 1-2

--- bonus/assets/js/main_b.js
+++ bonus/assets/js/main_b.js
@@ -11,22 +11,21 @@ const listElement = document.querySelector('ul.list');
 for (let i = 1; i <= 100; i++){
     // console.log(i);
     if (i % 3 === 0 && i % 5 === 0){
-        let liElement = `<li>FizzBuzz</li>`;
-        listElement.insertAdjacentHTML('beforebegin', liElement);
+        let liElement = `<li class="fizzbuzz">FizzBuzz</li>`;
+        listElement.insertAdjacentHTML('beforeend', liElement);
         // console.log('FizzBuzz');
     } else if (i % 3 == 0){
-        let liElement = `<li>Fizz</li>`;
-        listElement.insertAdjacentHTML('beforebegin', liElement);
+        let liElement = `<li class="fizz">Fizz</li>`;
+        listElement.insertAdjacentHTML('beforeend', liElement);
         // console.log('Fizz');
     } else if (i % 5 === 0){
-        let liElement = `<li>Buzz</li>`;
-        listElement.insertAdjacentHTML('beforebegin', liElement);
+        let liElement = `<li class="buzz">Buzz</li>`;
+        listElement.insertAdjacentHTML('beforeend', liElement);
         // console.log('Buzz');
     } else {
-        let liElement = `<li>${i}</li>`;
-        listElement.insertAdjacentHTML('beforebegin', liElement);
+        let liElement = `<li class="number">${i}</li>`;
+        listElement.insertAdjacentHTML('beforeend', liElement);
     }
-    
     
 }
 
