e8005dd2531db9eacc526a5ebc5de7b6d2b224d3
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-10 17:13:53 +0200

    final commit

--- bonus/assets/js/main_b.js
+++ bonus/assets/js/main_b.js
@@ -13,24 +13,28 @@ for (let i = 1; i <= 100; i++){
     if (i % 3 === 0 && i % 5 === 0){
         const ul = document.querySelector('ul.list');
         const li = document.createElement('li');
+        li.className = 'fizzbuzz'
         li.append('Fizzbuzz');
         ul.append(li);
         // console.log('FizzBuzz');
     } else if (i % 3 == 0){
         const ul = document.querySelector('ul.list');
         const li = document.createElement('li');
+        li.className = 'fizz'
         li.append('Fizz');
         ul.append(li);
         // console.log('Fizz');
     } else if (i % 5 === 0){
         const ul = document.querySelector('ul.list');
         const li = document.createElement('li');
+        li.className = 'buzz'
         li.append('Buzz');
         ul.append(li);
         // console.log('Buzz');
     } else {
         const ul = document.querySelector('ul.list');
         const li = document.createElement('li');
+        li.className = 'number'
         li.append(`${i}`);
         ul.append(li);
      
