a73c3d0d38c591371fb0e69d25c08fee52e1f4c2
giuliagrfo <giuliagrifo1@gmail.com>
2022-10-10 17:34:00 +0200

    fix code

--- bonus/assets/js/main_b.js
+++ bonus/assets/js/main_b.js
@@ -9,35 +9,28 @@
 const listElement = document.querySelector('ul.list');
 
 for (let i = 1; i <= 100; i++){
+    const ul = document.querySelector('ul.list');
+    const li = document.createElement('li');
     // console.log(i);
     if (i % 3 === 0 && i % 5 === 0){
-        const ul = document.querySelector('ul.list');
-        const li = document.createElement('li');
         li.className = 'fizzbuzz'
         li.append('Fizzbuzz');
         ul.append(li);
         // console.log('FizzBuzz');
     } else if (i % 3 == 0){
-        const ul = document.querySelector('ul.list');
-        const li = document.createElement('li');
         li.className = 'fizz'
         li.append('Fizz');
         ul.append(li);
         // console.log('Fizz');
     } else if (i % 5 === 0){
-        const ul = document.querySelector('ul.list');
-        const li = document.createElement('li');
         li.className = 'buzz'
         li.append('Buzz');
         ul.append(li);
         // console.log('Buzz');
     } else {
-        const ul = document.querySelector('ul.list');
-        const li = document.createElement('li');
         li.className = 'number'
         li.append(`${i}`);
         ul.append(li);
-     
     }
     
 }
